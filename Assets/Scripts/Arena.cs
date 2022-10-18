48969d37eec6336786c5e683738da1ea6134afd1
Selsanat <112934282+Selsanat@users.noreply.github.com>
2022-10-18 12:04:15 +0200

    Arena Avancements

---
+++ Assets/Scripts/Arena.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour
+{
+    public Transform HautGauche;
+    public Transform HautDroit;
+    public Transform BasGauche;
+    public Transform BasDroit;
+    public GameObject ennemi;
+    public GameObject Bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ennemi, SpawnMechant(HautGauche,HautDroit), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+//renvoie le Vector3 d'un point entre les deux transform donné
+    Vector3 SpawnMechant(Transform point1,Transform point2){
+        return(new Vector3(point1.position.x +Random.Range(0f,1f)*(point2.position.x -point1.position.x),point1.position.y+Random.Range(0f,1f)*(point2.position.y-point1.position.y)));
+    }
+}
