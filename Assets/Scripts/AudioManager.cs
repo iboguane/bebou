d6c305f672df430506630c472494507f307eea28
KorYme <maxime.corby.pro@gmail.com>
2022-10-18 12:14:49 +0200

    [ADD] : Many small features
    Playerstats + UI (Hearts + Timer) + TextMeshs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -69,3 +69,21 @@ public class AudioManager : MonoBehaviour
         s.source.Play();
     }
 }
+
+[System.Serializable]
+public class Sounds
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool soundtrack;
+    public string nextSoundName;
+
+    [HideInInspector]
+    public AudioSource source;
+}
