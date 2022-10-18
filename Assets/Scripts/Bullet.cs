036fadceb36ea7b8b84a45a1b90eb93e633ba42e
Selsanat <112934282+Selsanat@users.noreply.github.com>
2022-10-18 12:04:15 +0200

    Arena Avancements

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int speed;
+    private Vector3 player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player  , Time.deltaTime*speed);
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        //faire les dégats ici
+
+         Destroy(gameObject);
+    }
+}
