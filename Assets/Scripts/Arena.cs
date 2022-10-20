cc4e26c7000d39fa863f62e58a6ec65e86c6682f
Selsanat <leandro.benites@edu.devinci.fr>
2022-10-20 09:19:55 +0200

    [ADD] : Laser and arena spawns

--- Assets/Scripts/Arena.cs
+++ Assets/Scripts/Arena.cs
@@ -10,39 +10,52 @@ public class Arena : MonoBehaviour
     public Transform BasDroit;
     public GameObject ennemi;
     public GameObject Bullet;
-    public GameObject Lala;
+    public GameObject[] Ennemis;
     private GameObject ennemyRef;
     private int ran;
+    private int ranEnnemy;
+    private 
     // Start is called before the first frame update
     void Start()
     {
         //Instantiate(ennemi, SpawnMechant(HautGauche,HautDroit), Quaternion.identity);
+        StartCoroutine(Spawn());
     }
 
     // Update is called once per frame
     void Update()
     {
-        ran = Random.Range(0, 4);
-        if (ran == 0){
-            ennemyRef = Instantiate(ennemi, SpawnMechant(HautGauche, HautDroit), Quaternion.identity);
-        }
-        if (ran == 1)
-        {
-            ennemyRef = Instantiate(ennemi, SpawnMechant(HautGauche, BasGauche), Quaternion.identity);
-        }
-        if (ran == 2)
-        {
-            ennemyRef = Instantiate(ennemi, SpawnMechant(BasDroit, BasGauche), Quaternion.identity);
-        }
-        if (ran == 3)
-        {
-            ennemyRef = Instantiate(ennemi, SpawnMechant(BasDroit, HautDroit), Quaternion.identity);
-        }
-        Instantiate(Lala, ennemyRef.transform.position, Quaternion.identity);
     }
 
 //renvoie le Vector3 d'un point entre les deux transform donné
     Vector3 SpawnMechant(Transform point1,Transform point2){
         return(new Vector3(point1.position.x +Random.Range(0f,1f)*(point2.position.x -point1.position.x),point1.position.y+Random.Range(0f,1f)*(point2.position.y-point1.position.y)));
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            ran = Random.Range(0, 4);
+            //ranEnnemy = Random.Range(0, Ennemis.Length);
+            ranEnnemy = Random.Range(0, 1);
+            if (ran == 0)
+            {
+                ennemyRef = Instantiate(Ennemis[ranEnnemy], SpawnMechant(HautGauche, HautDroit), Quaternion.identity);
+            }
+            if (ran == 1)
+            {
+                ennemyRef = Instantiate(Ennemis[ranEnnemy], SpawnMechant(HautGauche, BasGauche), Quaternion.identity);
+            }
+            if (ran == 2)
+            {
+                ennemyRef = Instantiate(Ennemis[ranEnnemy], SpawnMechant(BasDroit, BasGauche), Quaternion.identity);
+            }
+            if (ran == 3)
+            {
+                ennemyRef = Instantiate(Ennemis[ranEnnemy], SpawnMechant(BasDroit, HautDroit), Quaternion.identity);
+            }
+            yield return new WaitForSeconds(1);
+        }
     }
 }
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,19 +6,34 @@ public class Bullet : MonoBehaviour
 {
     public int speed;
     private Vector3 player;
+    private Vector3 behindplayer;
+    private float d;
+    public float troploin;
     // Start is called before the first frame update
     void Start()
     {
+        transform.parent = null;
         player = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Transform>().position;
+        d = Mathf.Pow((this.gameObject.transform.position.x - player.x), 2) + Mathf.Pow((this.gameObject.transform.position.y - player.y), 2);
+        behindplayer = new Vector3(900 / d * player.x + (1 - 900 / d) * this.gameObject.transform.position.x, 900 / d * player.y + (1 - 900 / d) * this.gameObject.transform.position.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player  , Time.deltaTime*speed);
+        StartCoroutine(TueBullet());
+        transform.position = Vector3.MoveTowards(transform.position, behindplayer, Time.deltaTime*speed);
     }
     void OnTriggerEnter2D(Collider2D col){
         //PlayerStats.Instance.TakeDamage();
+        if (col.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator TueBullet()
+    {
+        yield return new WaitForSeconds(troploin);
         Destroy(gameObject);
     }
 }
