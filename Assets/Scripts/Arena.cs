d54e38b0d8cca22f7fd214245cbf774ce79ff395
Selsanat <leandro.benites@edu.devinci.fr>
2022-10-21 09:57:55 +0200

    [FIX] : Rotation projectiles and ennemies

--- Assets/Scripts/Arena.cs
+++ Assets/Scripts/Arena.cs
@@ -13,11 +13,13 @@ public class Arena : MonoBehaviour
     private int ran;
     private int ranEnnemy;
     private int IntervalSpawn =1;
+    private Vector3 player;
     // Start is called before the first frame update
     void Start()
     {
         //Instantiate(ennemi, SpawnMechant(HautGauche,HautDroit), Quaternion.identity);
         StartCoroutine(Spawn());
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
     }
 
     // Update is called once per frame
@@ -53,6 +55,9 @@ public class Arena : MonoBehaviour
             {
                 ennemyRef = Instantiate(Ennemis[ranEnnemy], SpawnMechant(BasDroit, HautDroit), Quaternion.identity);
             }
+            Vector3 difference = player - ennemyRef.transform.position;
+            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            ennemyRef.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ+180);
             yield return new WaitForSeconds(IntervalSpawn);
         }
     }
